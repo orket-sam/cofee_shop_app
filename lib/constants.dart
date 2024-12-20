3804560d00f3681c9a72dded2e2837e223eafaae
orket <developerorket@gmail.com>
2024-12-20 15:57:31 +0300

    lc

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,14 @@ const kBrandColorShade = Color(0xffedd6c8);
 const kBrandColorDark = Color(0xff313131);
 const kBrandLightGrey = Color(0x0ffe3e3e);
 const kBrandLightShade2 = Color(0xfff9f2ed);
+const kLightGreyText = Color(0xffa2a2a2);
 const kSBox20V = SizedBox(
   height: 20,
+);
+
+const kSBox10V = SizedBox(
+  height: 10,
+);
+const kSBox5V = SizedBox(
+  height: 5,
 );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:coffee_app/constants.dart';
 import 'package:coffee_app/presentation/screens/get_started_screen.dart';
-import 'package:coffee_app/presentation/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        appBarTheme: AppBarTheme(
+            titleTextStyle: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: Colors.black)),
         colorScheme: ColorScheme.fromSeed(seedColor: kBrandColor),
         useMaterial3: true,
       ),
